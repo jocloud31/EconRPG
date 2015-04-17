a0567f7ab0a4858e3929271056f0b127447cdb47
Notafryingpan_games <jocloud31@live.com>
2015-04-17 18:10:09 -0500

    First steps
    This is the first step. I'm planning to develop each aspect in a sandbox
    and integrate them closer to the end of the development process.

---
+++ Shop/Shop/Shop.py
@@ -0,0 +1,22 @@
+'''
+Created on Apr 14, 2015
+
+@author: Jay
+'''
+
+#shop element test for $unnamedRPG
+import pygame
+import random
+
+pygame.init()
+
+shopDisplay = pygame.display.set_mode((400,400))
+pygame.display.set_caption("Welcome to Jay's shop!")
+
+shopExit = False
+
+while not shopExit:
+    for event in pygame.event.get():
+        
+
+
